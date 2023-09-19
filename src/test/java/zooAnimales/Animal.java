ebe65f669b099bcd918203cd1394042f91e82794
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-09-19 11:07:15 -0500

    Update Animal.java

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -77,9 +77,32 @@ public class Animal {
 		return "Mamiferos: " + Mamifero.cantidadMamiferos()+"\n"+"Aves: " + Ave.cantidadAves()+"\n"+"Reptiles: " + Reptil.cantidadReptiles()+"\n"+"Peces: " + Pez.cantidadPeces()+"\n"+"Anfibios: "+ Anfibio.cantidadAnfibios();
 	}
 	
+	public String toString() {
+		if(zona.get(0).getZoo()!=null && zona!=null ) {
+			return "Mi nombre es "+ this.nombre+", tengo una edad de "+ this.edad +", habito en "+ this.habitat+ " y mi genero es "+ this.genero+",la zona en la que me ubico es "+ zona.get(0)+", en el "+ zona.get(0).getZoo();
+		}
+		else {
+			return "Mi nombre es "+ this.nombre+", tengo una edad de "+ this.edad +", habito en "+ this.habitat+ " y mi genero es "+ this.genero;
+		}
+	}
 	
-	
-	
+	public String movimiento() {
+		if(this instanceof Mamifero) {
+			return "desplazarse";
+		}
+		else if(this instanceof Ave) {
+			return "volar";
+		}
+		else if(this instanceof Reptil) {
+			return "reptar";
+		}
+		else if(this instanceof Pez) {
+			return "nadar";
+		}
+		else {
+			return "saltar";
+		}
+	}
 	
 
 }
