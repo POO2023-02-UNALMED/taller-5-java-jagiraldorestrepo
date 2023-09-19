31f130dfa5591ec7973e42dbb021e7121c276e07
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-09-19 11:21:40 -0500

    .

--- src/test/java/zooAnimales/Anfibio.java
+++ src/test/java/zooAnimales/Anfibio.java
@@ -42,5 +42,16 @@ public class Anfibio extends Animal{
 	public static int cantidadAnfibios() {
 		return listado.size();
 	}
+	
+	public static Animal crearRana(String nombre, int edad, String genero) {
+		ranas++;
+		return new Anfibio(nombre, edad,"selva",genero,"rojo",true);
+	}
+	public static Animal crearSalamandra(String nombre, int edad, String genero) {
+		salamandras++;
+		return new Anfibio(nombre, edad,"selva",genero,"negro y amarillo",false);
+	
 
+	
+	}
 }
