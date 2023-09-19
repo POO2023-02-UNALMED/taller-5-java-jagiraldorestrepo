c3876b1a552cc514120aaad2955716c5277b13f1
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-09-18 23:45:08 -0500

    2

--- src/test/java/gestión/Zona.java
+++ src/test/java/gestión/Zona.java
@@ -1,5 +1,21 @@
 package gestión;
 
+import java.util.ArrayList;
+
+
+
+
 public class Zona {
+	
+	private String nombre;
+	private ArrayList <Zoologico> zoo;
+	private ArrayList<Animal> animales;
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/test/java/gestión/Zoologico.java
+++ src/test/java/gestión/Zoologico.java
@@ -1,5 +1,17 @@
 package gestión;
 
+import java.util.ArrayList;
+
+
 public class Zoologico {
+	
+	private String nombre;
+	private String ubicacion;
+	private ArrayList<Zona> zonas;
+	
+	
+	
+	
+	
 
 }
