9bc2042020636238438444b4ee135a1fa5ec64af
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-09-19 12:03:50 -0500

    .

--- src/test/java/gestion/Zona.java
+++ src/test/java/gestion/Zona.java
@@ -6,21 +6,27 @@ import zooAnimales.*;
 
 public class Zona {
 	private String nombre;
-	private ArrayList <Zoologico> zoo;
+	//private ArrayList <Zoologico> zoo;
+	private Zoologico[] zoo = new Zoologico[1];
 	private ArrayList<Animal> animales;
 	
 	public Zona() {
 		this(null,null);
 	}
 	
-	public Zona(String nombre,Zoologico z) {
+	/*public Zona(String nombre,Zoologico z) {
 		this.nombre = nombre;
 		zoo=new ArrayList <>();
 		if (z!=null) {
 			zoo.add(z);
 		}
 		animales=new ArrayList<>();
-	}
+	}*/
+	
+    public Zona(String nombre, Zoologico zoo){
+        this.nombre = nombre;
+        this.zoo[0] = zoo;
+    }
 
 	public String getNombre() {
 		return nombre;
@@ -30,12 +36,20 @@ public class Zona {
 		this.nombre = nombre;
 	}
 
-	public Zoologico getZoo() {
+	/*public Zoologico getZoo() {
 		return zoo.get(0);
 	}
 
 	public void setZoo(Zoologico z) {
 		zoo.add(z);
+	}*/
+	
+	public Zoologico getZoo() {
+	    return zoo[0];
+	}
+
+	public void setZoo(Zoologico[] zoo) {
+	    this.zoo = zoo;
 	}
 
 	public ArrayList<Animal> getAnimales() {
@@ -53,4 +67,8 @@ public class Zona {
 	public int cantidadAnimales() {
 		return animales.size();
 	}
+	
+    public Zoologico getZoologico(){
+        return zoo[0];
+    }
 }
