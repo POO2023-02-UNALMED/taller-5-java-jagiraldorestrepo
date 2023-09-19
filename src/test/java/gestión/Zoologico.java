6ba179848bc7b3867e78011c15b901acd0df3472
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-09-19 10:35:24 -0500

    Update Zoologico.java

--- src/test/java/gestión/Zoologico.java
+++ src/test/java/gestión/Zoologico.java
@@ -2,6 +2,8 @@ package gestión;
 
 import java.util.ArrayList;
 
+import gestion.Zona;
+
 
 public class Zoologico {
 	
@@ -10,8 +12,39 @@ public class Zoologico {
 	private ArrayList<Zona> zonas;
 	
 	
+	public Zoologico() {
+		this(null,null);
+	}
 	
+	public Zoologico(String nombre, String ubicacion) {
+		this.nombre = nombre;
+		this.ubicacion = ubicacion;
+		zonas = new ArrayList<>();
+	}
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public ArrayList<Zona> getZona() {
+		return zonas;
+	}
+
+	public void setZona(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
 	
 
 }
